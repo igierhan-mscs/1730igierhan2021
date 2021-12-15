13218ecb2d33fdafffa4046c96f8d4bed38dc17a
iangierhan <iangierhan@gmail.com>
2021-12-14 19:15:03 -0600

    3c1 complete

---
+++ igierhan3c1/Ex3cCalculations.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace igierhan3c1
+{
+    public class Ex3cCalculations
+    {
+        public static string Calc0(int index)
+        {
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            for (int i = 0; i < days.Length; i++) ;
+                
+            return days.ToString();
+
+        }
+
+        public static string Calc1(string search)
+        {
+            string[] days = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+            string[] hours = { "Closed", "10am - 6pm", "10am - 6pm", "10am - 6pm", "10am - 9pm", "10am - 6pm", "8am - 4pm" };
+            search = search.Trim();
+            return "";
+        }
+
+        public static int Calc2(int[] numbers)
+        {
+            return 0;
+        }
+        public static double Calc3(double[] numbers, int count)
+        {
+            return 0.0;
+        }
+        public static double Calc5(double[] numbers)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < numbers.Length; i++)
+                sum += numbers[i];
+            double average = sum / numbers.Length;
+            return 0.0;
+        }
+
+        public static double[] Calc6(double[] numbers)
+        {
+            int length = numbers.GetLength(0);
+            List<double> aboveAvgList = new List<double>();
+            if (length > 0)
+            {
+                double avg = Calc5(numbers);
+
+                foreach (int total in aboveAvgList)
+                {
+                    if (total > avg)
+                        aboveAvgList.Add(total);
+                }
+            }
+
+            return aboveAvgList.ToArray();
+        }
+    }
+}
---
+++ igierhan3c1UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("igierhan3c1UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("igierhan3c1UnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a23c61c7-23f0-4367-80de-df6f9ca39056")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
